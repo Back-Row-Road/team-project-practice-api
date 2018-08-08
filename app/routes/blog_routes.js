3eae3f4ec073387a2f4f240c5b2ae0f6d9b17f16
Luis <luis.rodriguezcastro31@gmail.com>
2018-08-08 10:59:54 -0400

    nothing

--- app/routes/blog_routes.js
+++ app/routes/blog_routes.js
@@ -11,7 +11,7 @@ const Blog = require("../models/blog");
 
 const router = express.Router();
 
-router.get("/blogs", requireToken, (req, res) => {
+router.get("/blogs", (req, res) => {
   Blog.find()
     .then(blogs => {
       res.status(200).json(blogs);
